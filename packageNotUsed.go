095a75489855f5f0f59c0a68f9c92e3a079f83c7
Fanan Dala <fanan.dala@yahoo.com>
2020-03-28 17:58:16 +0100

    the underscore cheat for rule 1

--- packageNotUsed.go
+++ packageNotUsed.go
@@ -8,6 +8,7 @@ import (
 func main() {
 	/**
 		* Go does not allow you include packages you would not use
+		* This can be bypassed by placing an "_" before the package name
 	*/
 	fmt.Println("This can't compile because I included a package I did not use")
 }
