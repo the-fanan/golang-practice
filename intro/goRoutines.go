8165e72046f93eb9bb29ff4e5fb5f5d72e5ed25a
Fanan Dala <fanan.dala@yahoo.com>
2020-04-02 17:53:45 +0100

    understanding channels

--- intro/goRoutines.go
+++ intro/goRoutines.go
@@ -12,10 +12,47 @@ func countDown(x int, h string) {
 	countDown(x - 1, h)
 }
 
+/**
+* Order in which channels are written and read matters
+ * is chan s is sent a message before chan n then chan s message must be read before chan n
+*/
+func undChannels(x int, s chan int, n chan int) {
+	s <- x
+	n <- 1
+}	
+
+func pinger(c chan string) {
+	c <- "ping"
+}
+
+func printer(c chan string) {
+	msg := <- c
+	fmt.Println(msg)
+}
+
 func main() {
 	go countDown(5, "f")//run asyncrhonously
 	go countDown(6, "s")
-	var input string
-	fmt.Scanln(&input)
-	fmt.Println(input)
+
+	s := make(chan int)
+	n := make(chan int)
+	sum := 0
+
+	/*
+
+	for j := 0; j < 3; j++ {
+		go undChannels(j + 1,s,n)
+	}*/
+	var c chan string = make(chan string)
+	go pinger(c)
+	go printer(c)
+	go undChannels(5, s, n)
+	go undChannels(6, s, n)
+	go undChannels(9, s, n)
+	//since this block reads from channels, main will wait channel to be empty before closing
+	// remember, main is also a Go ROutine ;)
+	for i := 0; i < 3 ; i += <- n {
+		sum += <- s
+	}
+	fmt.Println("sum: ", sum)
 }
